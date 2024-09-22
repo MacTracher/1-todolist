a55818d7c09f571068b585488738c7cce2f2dd37
Oleg <kaspruk.oleg2002@gmail.com>
2024-09-22 10:44:21 -0500

    Lesson number 3 Done!

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,94 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import TodoList from "./TodoList";
+import TodoList, {TaskType} from "./TodoList";
+
+export type FilterValuesType = 'All' | 'Active' | 'Completed'
+
 
 function App() {
     const todoListTitle_1 = 'What to learn'
 
-    const task_1 = [
-        { id: 1, isDone: true, title: 'HTML&CSS' },
-        { id: 2, isDone: false, title: 'JS' },
-        { id: 3, isDone: true, title: 'React' },
+    // const tasks:any = [
+    //     { id: 1, isDone: true, title: 'HTML&CSS' },
+    //     { id: 2, isDone: false, title: 'JS' },
+    //     { id: 3, isDone: true, title: 'React' },
+    //
+    // ]
 
-    ]
+    const [tasks, setTasks] = useState(
+        [
+
+            {id: crypto.randomUUID(), isDone: true, title: 'HTML&CSS'},
+            {id: crypto.randomUUID(), isDone: false, title: 'JS'},
+            {id: crypto.randomUUID(), isDone: true, title: 'React'},
+
+        ])
+
+
+    const removeTask = (taskId: string) => {
+
+        // const nextState: Array<TaskType> = []
+        // for (let i = 0; i < tasks.length; i++) {
+        //     if (tasks[i].id !== taskId) {
+        //         nextState.push(tasks[i])
+        //     }
+        // }
+        setTasks(tasks.filter( t => t.id !== taskId))
+
+    }
+
+    const addTask = (newTaskTitle:string) => {
+        // const newTaskId = crypto.randomUUID()
+        // const newTask: TaskType  = {
+        //     id: newTaskId,
+        //     title: title,
+        //     isDone: false,
+        //
+        // }
+        // const nextState: Array<TaskType> = [newTask, ...tasks]
+        setTasks([{id: crypto.randomUUID(), title:newTaskTitle, isDone: false}, ...tasks])
+    }
+
+
+
+    const [filter, setFilter] = useState<FilterValuesType>('All')
+
+    const getFiltredTasksForRender = (allTasks: Array<TaskType>, filterValue: FilterValuesType): Array<TaskType> => {
+
+        switch (filterValue) {
+            case 'Active':
+                return allTasks.filter(t => !t.isDone )
+            case 'Completed':
+                return allTasks.filter(t => t.isDone )
+            default:
+                return allTasks
+        }
+    }
+
+
+    const ChangeFilter = (nextFilterValue: FilterValuesType) => {
+        setFilter(nextFilterValue)
+    }
+
+    const filtredTasksForRender: Array<TaskType> = getFiltredTasksForRender(tasks, filter)
+
+
+    // const callBack=(newTaskTitle:string, )=>{
+    //     setTasks([{id: crypto.randomUUID(), title:newTaskTitle, isDone: false}, ...tasks])
+    //
+    // }
+    //
+
     return (
         <div className="App">
-            <TodoList title={todoListTitle_1} tasks={task_1} />
+            <TodoList
+                title={todoListTitle_1}
+                tasks={filtredTasksForRender}
+                removeTask={removeTask}
+                ChangeFilter={ChangeFilter}
+                addTask={addTask}
+
+            />
 
         </div>
     );
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState, KeyboardEvent, ChangeEvent} from "react";
+import {FilterValuesType} from "./App";
+import {keyboardKey} from "@testing-library/user-event";
 
-type TaskType = {
-    id: number,
+export type TaskType = {
+    id: string,
     isDone: boolean,
     title: string,
 
@@ -10,44 +12,107 @@ type TaskType = {
 type TodoListPropsType = {
     title: string,
     tasks: Array<TaskType>,
+    removeTask: (taskId: string) => void,
+    ChangeFilter: (nextFilterValue: FilterValuesType) => void
+    addTask: (newTaskTitle: string) => void,
+
 
 }
 
 
-
-
-
-
-
 const TodoList = (props: TodoListPropsType) => {
 
-    const listItems = []
 
-    for ( let i = 0; i < props.tasks.length; i++){
-        const newListItem = <li>
-            <input type={"checkbox"} checked={props.tasks[i].isDone}/>
-            <span>{props.tasks[i].title}</span>
-            <button>x</button>
-        </li>
-        listItems.push(newListItem)
+    let {title, tasks, removeTask, ChangeFilter, addTask} = props;
+
+
+    // let taskList: Array<JSX.Element> | JSX.Element;
+    //
+    // if (tasks.length === 0) {
+    //     taskList = <span>Your TaskList is empty</span>
+    // } else {
+    //     const listItems = []
+    //
+    //     for (let i = 0; i < props.tasks.length; i++) {
+    //         const newListItem = <li>
+    //             <input type={"checkbox"} checked={tasks[i].isDone}/>
+    //             <span>{tasks[i].title}</span>
+    //             <button>x</button>
+    //         </li>
+    //         listItems.push(newListItem)
+    //     }
+    //     taskList = <ul>
+    //         {listItems}
+    //     </ul>
+    // }
+
+    // const RemoveButtonHandler=(id: number ) => {
+    //     return props.removeTask(id)
+    // }
+
+
+    const listItems: Array<JSX.Element> =
+        tasks.map(t => {
+            return (
+                <li key={t.id}>
+                    <input type={"checkbox"} checked={t.isDone}/>
+                    <span>{t.title}</span>
+                    <button onClick={() => removeTask(t.id)}>x</button>
+                </li>
+            )
+        })
+
+
+    let [newTaskTitle, setNewTaskTitle] = useState("")
+
+
+    const onClickAddTask = () => {
+        addTask(newTaskTitle)
+        setNewTaskTitle('')
     }
+
+    const isAddBtnDisabled = !newTaskTitle || newTaskTitle.length >= 15
+
+    const userMessage = newTaskTitle.length >= 15
+        ? <span style={{color: "darkred"}}>Your task title is too long</span>
+        : <span>Enter new task title</span>
+
+    const onKeyDownAddTask = (event: KeyboardEvent<HTMLInputElement>) => event.key === 'Enter' && onClickAddTask()
+
+    const onChangeSetNewTaskTitle = (e: ChangeEvent<HTMLInputElement>) => setNewTaskTitle(e.target.value)
+
+    // props.setTasks([{id: crypto.randomUUID(), title:newTaskTitle, isDone: false}, ...tasks])
+
+
+    const taskList: Array<JSX.Element> | JSX.Element = !tasks.length ? <span>Your TaskList is empty</span> :
+        <ul>{listItems}</ul>
 
 
     return (
         <div className="todolist">
             <h3>{props.title}</h3>
             <div>
-                <input/>
-                <button>+</button>
-            </div>
-            <ul>
-                {listItems}
+                <input
+                    value={newTaskTitle}
+                    onChange={onChangeSetNewTaskTitle}
+                    onKeyDown={onKeyDownAddTask}
 
-            </ul>
+                />
+                <button
+                    onClick={onClickAddTask}
+
+                    disabled={isAddBtnDisabled}
+                >+
+                </button>
+                <div>
+                    {userMessage}
+                </div>
+            </div>
+            {taskList}
             <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <button onClick={() => ChangeFilter('All')}>All</button>
+                <button onClick={() => ChangeFilter('Active')}>Active</button>
+                <button onClick={() => ChangeFilter('Completed')}>Completed</button>
             </div>
         </div>
     )
